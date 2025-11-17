576de04dd75403777ce39aaf35b0d278c4838f8b
megoodev <mr.hossammostafa2023@gmail.com>
2025-11-17 12:49:16 +0200

    fix course route page

--- src/app/(public)/courses/page.tsx
+++ src/app/(public)/courses/page.tsx
@@ -4,6 +4,7 @@ import PublicCardCouese, {
 import { Suspense } from "react";
 import { getAllcourses } from "@/data/course/get-all-courses";
 
+export const dynami = "force-dynamic"
 export default async function PublicCourseRoute() {
   return (
     <div className="mt-2">
