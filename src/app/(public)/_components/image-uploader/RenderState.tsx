960a581893b1002045deecf14f465deddb90bc48
hossam mostfa <mr.hossammostfa2023@gmail.com>
2025-11-21 01:14:05 +0200

    last change

--- src/app/(public)/_components/image-uploader/RenderState.tsx
+++ src/app/(public)/_components/image-uploader/RenderState.tsx
@@ -81,7 +81,7 @@ export const RenderUploadedState = ({
       >
         <XIcon className="size-4" />
       </Button>
-      <div className="relative flex items-center mx-auto justify-center max-h-[100%] max-w-[100%] h-[80%]">
+      <div className="relative flex items-center mx-auto justify-center max-h-[100%] max-w-[100%] size-60 object-contain">
         {isImage ? (<Image src={url} alt={url} fill className="object-contain" />): ( <video src={url} controls className="w-full h-full object-contain"/>)}
         
       </div>
